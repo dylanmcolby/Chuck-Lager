0fd52513569c92bfc7835c34f29fa83c488cc234
dylanmcolby <66750822+dylanmcolby@users.noreply.github.com>
2023-09-26 16:58:27 -0500

    update to use ipinfo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     function getCookie(name) {
         var value = "; " + document.cookie;
         var parts = value.split("; " + name + "=");
@@ -7,44 +7,45 @@ $(document).ready(function () {
     }
 
     const setLocation = function () {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var userLat = position.coords.latitude;
-                var userLng = position.coords.longitude;
-                var tempDiv = $("<div>");
-                tempDiv.load("/locations #addresses", function () {
-                    var locations = [];
-                    tempDiv.find('.geo-location').each(function () {
-                        var location = {
-                            lat: parseFloat($(this).find('.geo-lat').text()),
-                            lng: parseFloat($(this).find('.geo-long').text()),
-                            address: $(this).find('.geo-address').text(),
-                            slug: $(this).find('.geo-slug').text(),
-                            location: $(this).find('.geo-location').text()
-                        };
-                        locations.push(location);
-                    });
+        $.get("https://ipinfo.io", function (response) {
+            var loc = response.loc.split(','); 
+            var userLat = parseFloat(loc[0]);
+            var userLng = parseFloat(loc[1]);
+            var tempDiv = $("<div>");
 
-                    var closestLocation = null;
-                    var shortestDistance = Number.MAX_VALUE;
-
-                    locations.forEach(function (location) {
-                        var distance = Math.sqrt(Math.pow(location.lat - userLat, 2) + Math.pow(location.lng - userLng, 2));
-                        if (distance < shortestDistance) {
-                            shortestDistance = distance;
-                            closestLocation = location;
-                        }
-                    });
-                    document.cookie = "restaurantLocation=" + closestLocation.location; + "; path=/";
-                    document.cookie = "restaurantSlug=" + closestLocation.slug; + "; path=/";
-                    $('#nav-location-name').text(closestLocation.location);
-                    $('#nav-location-link').attr('href', 'locations/' + closestLocation.slug);
-
+            tempDiv.load("/locations #addresses", function () {
+                var locations = [];
+                tempDiv.find('.geo-location').each(function () {
+                    var location = {
+                        lat: parseFloat($(this).find('.geo-lat').text()),
+                        lng: parseFloat($(this).find('.geo-long').text()),
+                        address: $(this).find('.geo-address').text(),
+                        slug: $(this).find('.geo-slug').text(),
+                        location: $(this).find('.geo-location').text()
+                    };
+                    locations.push(location);
                 });
+
+                var closestLocation = null;
+                var shortestDistance = Number.MAX_VALUE;
+
+                locations.forEach(function (location) {
+                    var distance = Math.sqrt(Math.pow(location.lat - userLat, 2) + Math.pow(location.lng - userLng, 2));
+                    if (distance < shortestDistance) {
+                        shortestDistance = distance;
+                        closestLocation = location;
+                    }
+                });
+
+                document.cookie = "restaurantLocation=" + closestLocation.location + "; path=/";
+                document.cookie = "restaurantSlug=" + closestLocation.slug + "; path=/";
+                $('#nav-location-name').text(closestLocation.location);
+                $('#nav-location-link').attr('href', '/locations/' + closestLocation.slug);
+
             });
-        }
+        }, "jsonp");
     };
-    
+
     if (!getCookie("restaurantLocation") || !getCookie("restaurantSlug")) {
         setLocation();
     } else {
