969b61f66104df4724ee4d073a6071ef1d3fb56d
dylanmcolby <66750822+dylanmcolby@users.noreply.github.com>
2023-09-26 16:28:33 -0500

    remove timeout, set as function

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 $(document).ready(function () {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var userLat = position.coords.latitude;
-            var userLng = position.coords.longitude;
-            var tempDiv = $("<div>");
-            tempDiv.load("/locations #addresses", function () {
-                var locations = [];
-                tempDiv.find('.geo-location').each(function () {
-                    var location = {
-                        lat: parseFloat($(this).find('.geo-lat').text()),
-                        lng: parseFloat($(this).find('.geo-long').text()),
-                        address: $(this).find('.geo-address').text(),
-                        slug: $(this).find('.geo-slug').text(),
-                        location: $(this).find('.geo-location').text()
-                    };
-                    locations.push(location);
-                });
 
-                var closestLocation = null;
-                var shortestDistance = Number.MAX_VALUE;
+    const setLocation = function () {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var userLat = position.coords.latitude;
+                var userLng = position.coords.longitude;
+                var tempDiv = $("<div>");
+                tempDiv.load("/locations #addresses", function () {
+                    var locations = [];
+                    tempDiv.find('.geo-location').each(function () {
+                        var location = {
+                            lat: parseFloat($(this).find('.geo-lat').text()),
+                            lng: parseFloat($(this).find('.geo-long').text()),
+                            address: $(this).find('.geo-address').text(),
+                            slug: $(this).find('.geo-slug').text(),
+                            location: $(this).find('.geo-location').text()
+                        };
+                        locations.push(location);
+                    });
 
-                locations.forEach(function (location) {
-                    var distance = Math.sqrt(Math.pow(location.lat - userLat, 2) + Math.pow(location.lng - userLng, 2));
-                    if (distance < shortestDistance) {
-                        shortestDistance = distance;
-                        closestLocation = location;
-                    }
-                });
+                    var closestLocation = null;
+                    var shortestDistance = Number.MAX_VALUE;
 
-                setTimeout(function () {
+                    locations.forEach(function (location) {
+                        var distance = Math.sqrt(Math.pow(location.lat - userLat, 2) + Math.pow(location.lng - userLng, 2));
+                        if (distance < shortestDistance) {
+                            shortestDistance = distance;
+                            closestLocation = location;
+                        }
+                    });
                     alert("The closest location is: " + closestLocation.location);
-                }, 500);
+                });
             });
-        });
-    }
+        }
+    };
+    setLocation();
+
 });
