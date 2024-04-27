88dd685d678948d89a45ff93f14f0c8ab025113e
dylanmcolby <66750822+dylanmcolby@users.noreply.github.com>
2024-04-27 09:53:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -520,57 +520,57 @@ document.addEventListener('DOMContentLoaded', function () {
     //
     //RICH TEXT MENU SETUP 
     //
-    function updateMenuItems() {
-        $('.menu_items-rich-text').each(function () {
-            const icons = {
-                '[glutenfree]': '<img alt="Gluten Free" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/65148360a9e331145818522c_glutenfree.svg">',
-                '[spicy]': '<img alt="Spicy" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/651483615ff4e3a188eb7155_hot.svg">',
-                '[regional]': '<img alt="Regional" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/651484938f6dafebbddc75f9_local.svg">'
-            };
+    setTimeout(() => {
 
-            Object.keys(icons).forEach(key => {
-                const regex = new RegExp(key, 'gi'); // Case insensitive
-                const replacement = icons[key];
-                $(this).html($(this).html().replace(regex, replacement));
-            });
-        });
+    $('.menu_items-rich-text').each(function () {
+        // Replace [glutenfree] with its corresponding image
+        var glutenFreeHTML = '<img alt="Gluten Free" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/65148360a9e331145818522c_glutenfree.svg">';
+        $(this).html($(this).html().replace(/\[glutenfree\]/g, glutenFreeHTML));
+        $(this).html($(this).html().replace(/\[GlutenFree\]/g, glutenFreeHTML));
 
-        $('.menu_categories').each(function () {
-            const itemsCount = $(this).children().length;
-            if (itemsCount < 2) {
-                $(this).addClass('one-column');
-            } else if (itemsCount < 4) {
-                $(this).addClass('two-column');
+        // Replace [spicy] with its corresponding image
+        var spicyHTML = '<img alt="Spicy" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/651483615ff4e3a188eb7155_hot.svg">';
+        $(this).html($(this).html().replace(/\[spicy\]/g, spicyHTML));
+        $(this).html($(this).html().replace(/\[Spicy\]/g, spicyHTML));
+
+        // Replace [regional] with its corresponding image
+        var regionalHTML = '<img alt="Regional" style="display:inline-block;margin-top:-.25rem" class="icon-1x1-xsmall" src="https://uploads-ssl.webflow.com/6501f8d7518f57ff9967db13/651484938f6dafebbddc75f9_local.svg">';
+        $(this).html($(this).html().replace(/\[regional\]/g, regionalHTML));
+        $(this).html($(this).html().replace(/\[Regional\]/g, regionalHTML));
+    });
+
+
+    let checkExist = setInterval(function () {
+        if (typeof fsAttributes !== 'undefined') {
+            clearInterval(checkExist); // stop the interval
+            if (fsAttributes && fsAttributes.cmsnest && fsAttributes.cmsnest.loading) {
+                fsAttributes.cmsnest.loading.then(function (result) {
+                    $('.menu_categories').each(function (index) {
+                        const itemsCount = $(this).children().length;
+
+                        if (itemsCount < 2) {
+                            $(this).addClass('one-column');
+                        } else if (itemsCount < 4) {
+                            $(this).addClass('two-column');
+                        } else {
+                            $(this).removeClass('two-column');
+                            $(this).removeClass('one-column');
+                        }
+                    });
+                }).catch(function (error) {
+                    console.log('Promise rejected with error:', error);
+                });
             } else {
-                $(this).removeClass('one-column two-column');
+                console.log('fsAttributes or its properties do not exist.');
             }
-        });
-    }
+        }
+    }, 100); // check every 100ms
 
-    // Run once on initial load
-    updateMenuItems();
-
-    // Set up a mutation observer to watch for changes in menu_categories elements
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            if (mutation.type === 'childList' || mutation.type === 'subtree') {
-                updateMenuItems();
-            }
-        });
-    });
-
-    const observerOptions = {
-        childList: true,
-        subtree: true
-    };
-
-    document.querySelectorAll('.menu_categories').forEach(targetNode => {
-        observer.observe(targetNode, observerOptions);
-    });
-
-    // Optional: To disconnect the observer after a certain period, uncomment the following line:
-    // setTimeout(() => observer.disconnect(), 10000);
-});
+    // Stop checking after 10 seconds
+    setTimeout(function () {
+        clearInterval(checkExist);
+    }, 10000);
+ }, 1000);
 
     //HIDE HOURS THAT AREN'T TODAY
     // Get the current day of the week
@@ -593,5 +593,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
-
-
